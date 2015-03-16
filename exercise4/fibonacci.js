d0ec1d347ad37e0d451e2a24650a041a2da6f546
flymop <hftangyang@gmail.com>
2015-03-17 00:50:12 +0800

    correct

--- exercise4/fibonacci.js
+++ exercise4/fibonacci.js
@@ -1,5 +1,17 @@
-var fib = function (i) {
-	if(i<2) return 1;
-	return fib(i-1) + fib(i-2);
+var fib = function (n) {
+	var fibonacciNum=0;
+	//a,b means two add number 
+	function calNum(n) {
+   if(n>3) return calNum(n-1) + calNum(n-2);
+   else return 1;
+
+	}
+	fibonacciNum = calNum(n,0,1);
+	return function() {
+		return fibonacciNum; 
+	}
 }
+
+var fib1 = fib(5);
+console.log(fib1());
 
